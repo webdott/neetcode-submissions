17cce7addc9a474c1df890df7483e2580417879b
Uchechukwu Nwafor <nwaforuchechukwu2007@gmail.com>
2026-07-13 19:51:09 +0100

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.ans = []
+
+        self.backtrack(n * 2, [], 0, 0)
+
+        return self.ans
+    
+    def backtrack(self, n:int, curr: List[str], front: int, back: int):
+        if back > front or front > n / 2:
+            return;
+
+        if front + back == n:
+            if front == back:
+                self.ans.append("".join(curr[:]))
+                
+            return
+
+        curr.append("(")
+        self.backtrack(n, curr, front + 1, back)
+        curr.pop()
+
+        curr.append(")")
+        self.backtrack(n, curr, front, back + 1)
+        curr.pop()
