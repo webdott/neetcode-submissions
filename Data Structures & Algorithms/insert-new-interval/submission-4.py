57c3056beeb8d672c52efc83062a2acb0ae24911
Uchechukwu Nwafor <nwaforuchechukwu2007@gmail.com>
2026-07-28 13:33:50 +0100

    Add: insert-new-interval - submission-4

---
+++ Data Structures & Algorithms/insert-new-interval/submission-4.py
@@ -0,0 +1,42 @@
+class Solution:
+    def isOverlapping(self, a, b, c, d) -> bool:
+        return (b <= d and c <= b) or (d <= b and a <= d)
+
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        if not intervals:
+            return [newInterval]
+
+        if newInterval[1] < intervals[0][0]:
+            return [newInterval] + intervals
+
+        if newInterval[0] > intervals[-1][1]:
+            return intervals + [newInterval]
+            
+        ans, seen = [intervals[0]], False
+
+        for a, b in intervals:
+            c, d = ans[-1]
+
+            if self.isOverlapping(a, b, c, d):
+                ans[-1] = [min(a, c), max(b, d)]
+            else:
+                ans.append([a, b])
+
+            e, f = ans[-1]
+
+            if not seen:
+                g, h = newInterval
+                
+                if self.isOverlapping(e, f, g, h):
+                    ans[-1] = [min(e, g), max(f, h)]
+                    seen = True
+                elif h < e:
+                    t = ans.pop()
+                    ans.append([g, h])
+                    ans.append(t)
+                    seen = True
+
+        return ans
+
+
+        
