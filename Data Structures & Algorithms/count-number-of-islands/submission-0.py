6edcc1a0040c6e2ebfdc546679853db9a2ef6475
Uchechukwu Nwafor <nwaforuchechukwu2007@gmail.com>
2026-08-20 14:55:29 +0100

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -0,0 +1,37 @@
+class Solution:
+    def getNeighbors(self, row: int, col: int) -> List(Tuple):
+        return [
+            (row - 1, col),
+            (row, col + 1),
+            (row + 1, col),
+            (row, col - 1)
+        ]
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        n, m = len(grid), len(grid[0])
+        seen = set()
+
+        def isIsland(row: int, col: int) -> bool:
+            if row >= n or col >= m or row < 0 or col < 0 or grid[row][col] == "0" or (row, col) in seen:
+                return False
+
+            seen.add((row, col))
+
+            nxt = self.getNeighbors(row, col)
+
+            for r, c in nxt:
+                isIsland(r, c)
+
+            return True
+
+        res = 0
+
+        for r in range(n):
+            for c in range(m):
+                if grid[r][c] == "0" or (r, c) in seen:
+                    continue
+
+                if isIsland(r, c):
+                    res += 1
+
+        return res
