23b5754defb0375b99541dae041b5eb9cd666685
Uchechukwu Nwafor <nwaforuchechukwu2007@gmail.com>
2026-08-26 23:49:20 +0100

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,41 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(set)
+        s, v = set(), {}
+
+        for r, c in prerequisites:
+            if r == c:
+                return False
+
+            adj[r].add(c)
+            
+            if c not in adj:
+                adj[c] = set()
+
+        def dfs(i) -> bool:
+            if i in v:
+                return True
+                
+            if len(adj[i]) == 0:
+                return True
+
+            if i in s:
+                return False
+
+            s.add(i)
+
+            for x in adj[i]:
+                if x in s or not dfs(x):
+                    return False
+
+            s.remove(i)
+
+            v[i] = True
+            return True
+
+        for x in adj:
+            if not dfs(x):
+                return False
+
+        return True
+        
